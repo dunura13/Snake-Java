3b7c7d1703f75869117cf85381dab69ba918e070
Dunura E <115491008+dunura13@users.noreply.github.com>
2024-01-06 14:15:19 -0500

    Updated GameFrame.java

--- SnakeGameJava/GameFrame.java
+++ SnakeGameJava/GameFrame.java
@@ -3,33 +3,25 @@ import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
 
-
-    // Constructor
     GameFrame(){
-
-        // CREATING AN INSTANCE OF OUR GAME PANEL and using JFrame's methods
         GamePanel panel = new GamePanel();
-        this.add(panel); // we use this to add the visual components to our game
+        this.add(panel);
 
         // set the title
         this.setTitle("Snake");
         
-        // closes game when we press close
+        // closes game
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // this would allow us to resize the window
+        // resize the window
         this.setResizable(false);
         
-        //allows us to use other components around just Jframe
         this.pack();
 
-        // this allows us to see the window
+        // see the window
         this.setVisible(true);
 
         // this makes the window appear in the middle of the screen
         this.setLocationRelativeTo(null);
-
     }
-
-    
 }
